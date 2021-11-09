2e6e14f170c0799a56ea92537df093b08e2bcf61
ebru_ <ebru_@DESKTOP-24TS0QH>
2021-11-09 21:43:03 +0300

    Add project files.

---
+++ DamaTahtası/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamaTahtası
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 8; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    if (i % 2 == 0)
+                        Console.Write(" *");
+                    else
+                        Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            
+
+            Console.ReadLine();
+        }
+        
+    }
+}
